db3421c10b44a0cd3a41ec7f90b590f6160be811
Taylor Sperry <sperry.taylor@gmail.com>
2019-04-11 23:01:25 -0600

    begin tests

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+// import { fetchRights } from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  describe('getRights', () => {
+    
+    it('should call fetchRights if there are no rights in the store', () => {
+      const wrapper = shallow(<App />)
+      wrapper.instance().getRights()
+      // wrapper.instance().getRights()
+      // expect(fetchRights).toHaveBeenCalled()
+    })
+  })
+
+})
+
