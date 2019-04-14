55d4c775e5bdcb52e7ec8f113f110e8616baa15b
Taylor Sperry <sperry.taylor@gmail.com>
2019-04-14 17:31:35 -0600

    add border, find nearest coord, and calculate dist

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { Map as LeafletMap, TileLayer, Marker, GeoJSON } from 'react-leaflet'
 import { connect } from 'react-redux'
-import { coordinates } from '../../helpers/geojson'
 import './_Location.scss';
 
 export class Location extends Component {
@@ -9,13 +8,15 @@ export class Location extends Component {
     super();
     this.state = {
       location: [40.650002, -73.949997],
-      location2: [],
-      distance: 0
+      nearestPt: [],
+      distance: 0,
+      border: [],
     }
   }
 
   componentDidMount() {
     this.getLocation()
+    this.getBorder()
   }
 
   getLocation = () => {
@@ -28,52 +29,84 @@ export class Location extends Component {
     })
   }
 
-  getDistance = () => {
+  findNearest = () => {
     let geodist = require('geodist')
-    let currLocation = this.state.location
-    let location2 = this.state.location2
-    return geodist(currLocation, location2, {exact: true, unit: 'mi'})
+    let { border, location } = this.state
+    let shortestDist = border.map(coord => ({
+      coord : coord,
+      dist: geodist(location, coord, {exact: true, unit: 'mi'})
+    })).sort((a,b) => a.dist-b.dist)[0]
+    this.setData(shortestDist)
+  }
+
+  setData = (shortestDist) => {
+    let distance = Math.ceil(shortestDist.dist)
+    let cleanCoords= shortestDist.coord.map(coord => Math.ceil(coord))
+    this.setState({
+      nearestPt: cleanCoords,
+      distance: distance
+    })
   }
 
   addMarker = (e) => {
     let {lat, lng} = e.latlng
     this.setState({
-      location2: [lat, lng]
+      nearestPt: [lat, lng]
     })
+  }
+
+  getBorder = async () => {
+    const url = 'https://services1.arcgis.com/Hp6G80Pky0om7QvQ/arcgis/rest/services/Mexico_and_US_Border/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      const coordinates = data.features[0].geometry.paths[0]
+      this.setState({
+        border: coordinates
+      })
+    } catch (error) {
+      console.log(error.message)
+    }
   }
   
   render() {
-    let location2
-    let distance 
-    let msg = 'click the border to calculate distance'
-    if(this.state.location2.length) {
-      location2 = <Marker position={this.state.location2} />
-      distance = this.getDistance()
-      msg = `you are ${distance} miles from the border`
-    }
-
+    let nearestPt
+    let msg
+    let border 
     let data = {
       "type": "LineString",
-      "coordinates": coordinates
+      "coordinates": this.state.border,
+    }
+  
+    if(this.state.nearestPt.length) {
+      nearestPt = <Marker position={this.state.nearestPt} />
+    }
+
+    if(this.state.border.length) {
+      border = <GeoJSON 
+            data={data}
+          />
+    }
+
+    if(this.state.distance) {
+      msg=`You are ${this.state.distance} miles from the border`
     }
     
     return (
       <div className='map-container'>
-        <p>{msg}</p>
+        <button onClick={this.findNearest}>Calculate Distance</button>
+        {msg}
         <LeafletMap
           id='map'
           center={this.state.location}
           zoom='4'
-          onClick={this.addMarker}
         >
-          <GeoJSON 
-            data={data}
-          />
+          {border}
           <TileLayer 
             url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
           />
           <Marker position={this.state.location} />
-          {location2}
+          {nearestPt}
         </LeafletMap>
       </div>
     )
